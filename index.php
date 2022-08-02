8468796e5f1830bd8c5eb63d6cd133e04dbd8515
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-08-02 11:55:54 -0300

    mobile
    hearder
    section

--- index.php
+++ index.php
@@ -5,8 +5,114 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto - back end</title>
+
+    <!-- Css includes -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Css proprio -->
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <!-- Icons Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
 </head>
 <body>
-    
+    <header class="d-flex justify-content-around">
+        <h1>
+            <img src="#" alt="logo" title="">
+        </h1>
+
+        <nav class="navbar">
+            <button class="btn btn-sm btn-outline-secondary mx-1" type="button">Login</button>
+            <button class="btn btn-sm btn-outline-secondary mx-1" type="button">Cadastro</button>
+        </nav>
+    </header>
+
+    <section class="container my-5 justify-content-center">
+        <picture class="d-flex justify-content-center">
+            <source media="(min-width:650px)" srcset="img/img-projetando.png">
+              
+            <img class="d-flex w-75 m-auto" src="img/img-projetando.png" alt="banner tudo o que você precisa esta aqui">
+        </picture>
+
+        <div class="align-center p-3 text-center">
+            <h1>
+                Lorem ipsum dolor sit amet.
+            </h1>
+
+            <h2>
+                Lorem ipsum dolor.
+            </h2>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium nesciunt aliquam atque similique inventore quibusdam, consequuntur esse unde rem quae ducimus distinctio nostrum ad rerum.
+            </p>
+        
+            <button class="btn btn-sm btn-outline-secondary mt-3 px-5 align-center rounded-pill" type="button">Começa agora!</button>
+        </div>
+    </section>
+
+
+    <section class="container-flex">
+        <img class="d-flex w-75 m-auto" src="img/img-equip-desenvolvendo.png" alt="banner tudo o que você precisa esta aqui">
+
+        <article class="align-center p-3 text-center bg-verde pt-5 imgcasal">
+            <h2>
+                Comece agora!
+            </h2>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+            </p>
+        
+            <button class="btn btn-sm btn-outline-marinho mt-3 px-5 align-center rounded-pill" type="button">Saiba +</button>
+        </article>
+    </section>
+
+    <section class="container">
+        <div class="row m-1">
+
+            <h2 class="text-center my-5">Encontre Freelancers</h2>
+
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>

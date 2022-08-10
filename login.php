2fc42566b49e522b2ae7a795fae99d176d896a8e
Gabriel Genovez <86001324+GabrielG9z@users.noreply.github.com>
2022-08-10 11:46:02 -0300

    Update
    Criando a página de login

---
+++ login.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Css includes -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Css proprio -->
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <!-- Icons Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <title>login</title>
+</head>
+<body>
+    <main class="container text-center">
+            <div class="row align-items-center">
+            <div class="col">
+                <h1>XPTO.com</h1>
+                <h4>Entre na XPTO, e contemple nossos beneficíos</h4>
+            
+             <p>Aqui você encontra a soluçao dos seus problemas</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo nostrum aliquam voluptatibus. Cumque, nulla?</p>
+            
+                
+
+                <p><img src="img/img-equip-desenvolvendo.png" alt=""></p>
+            </div>
+            <div class="col">
+            <form>
+                <div class="mb-3">
+                <p><a href="#">Google</a></p>
+                <p><a href="#"></a></p>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+            </div>
+            
+        </div>
+    </main>
+
+</body>
+</html>

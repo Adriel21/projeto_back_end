8ac6f11699e9d535f11e4c15376dcc623c369531
GabrielG9z <genovezgabriel2012@gmail.com>
2022-08-10 20:27:20 -0300

    Foram feitas correções no index
    Os conflitos foram solucionados, e foram adicionadas novas imagens ao projeto, junto de cards mais atuais, algumas linhas de css que estavam atrapalhando foram excluidas, pra que novas sejam feitas

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php 
 require_once 'inc/cabecalho_externo.php'
  
@@ -18,13 +17,13 @@ require_once 'inc/cabecalho_externo.php'
   
         <div class="row align-items-center ">
 
-        <section class="col d-none d-md-block d-print-block col-md-6">
-           <h1>Comece agora!</h1>
-           <h3>Com apenas um clique</h3>
-            <button type="submit">Clica-ae</button>
-        </section>
+            <section class="col d-none d-md-block d-print-block ">
+                <h1>Comece agora!</h1>
+                <h3>Com apenas um clique</h3>
+                <button type="submit">Clica-ae</button>
+            </section>
         
-        <section class="col d-none d-md-block d-print-block col-md-6">
+        <section class="col d-none d-md-block d-print-block ">
             <p><img src="img/img-job-anotacao.png" alt=""></p>
         </section>
     </div>
@@ -39,65 +38,6 @@ require_once 'inc/cabecalho_externo.php'
         <div class="align-center p-3 text-center d-md-none">
             <h1>
                 Lorem ipsum dolor sit amet.
-=======
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto - back end</title>
-
-    <!-- Css includes -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-    <!-- Css proprio -->
-    <link rel="stylesheet" href="css/estilo.css">
-
-    <!-- Icons Bootstrap -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-
-</head>
-<body>
-    <header class="d-flex justify-content-around">
-        <h1>
-            <img src="#" alt="logo" title="">
-        </h1>
-
-        <nav class="navbar">
-            <button class="btn btn-sm btn-outline-secondary mx-1" type="button">Login</button>
-            <button class="btn btn-sm btn-outline-secondary mx-1" type="button">Cadastro</button>
-        </nav>
-    </header>
-
-    <section class="container my-5 justify-content-center">
-
-        <div class="container text-center">
-  <div class="row">
-    <div class="col">
-      <h1>Comece agora!</h1>
-      <h2>apenas com um clique</h2>
-      <p><button type="button" class="btn btn-primary">Start</button></p>
-    </div>
-    <div class="col">
-    
-      <p><img src="img/img-equip-desenvolvendo.png" alt="Bonequinho mexendo no pc"></p>
-    </div>
-  </div>
-
-  <div class="container text-center">
-  <div class="row">
-    <div class="col">
-      1 of 2
-    </div>
-    <div class="col">
-      2 of 2
-    </div>
-  </div>
-        <div class="align-center p-3 text-center">
-            <h1>
-                Starting with our platform
->>>>>>> Stashed changes
             </h1>
 
             <h2>
@@ -117,21 +57,19 @@ require_once 'inc/cabecalho_externo.php'
 
     <section class="container text-center">
 
-    <div class="row align-items-center ">
-        <div class="row">
-            <div class="col align-self-center d-none d-md-block d-print-block ">
-                <p><img src="img/img-equip-desenvolvendo.png" alt="Equipe desenvolvendo"></p>
-            </div>
-    
-            <div class="col align-self-start d-none d-md-block d-print-block ">
-                <h1>Call To Action</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem velit quam, hic vero expedita rerum. Quibusdam consectetur temporibus omnis natus.</p>
-                <button type="submit">Clica-ae</button>
-            </div>
-        </div>
-    </div>
-             
+        <div class="row align-items-center ">
+            <div class="row">
+                <div class="col align-self-center d-none d-md-block d-print-block ">
+                    <p><img src="img/img-equip-desenvolvendo.png" alt="Equipe desenvolvendo"></p>
+                </div>
         
+                <div class="col align-self-start d-none d-md-block d-print-block ">
+                    <h1>Call To Action</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem velit quam, hic vero expedita rerum. Quibusdam consectetur temporibus omnis natus.</p>
+                    <button type="submit">Clica-ae</button>
+                </div>
+            </div>
+        </div>     
     </section>
 
     <!-- Final segundo destaque desktop/mobile -->
@@ -139,106 +77,84 @@ require_once 'inc/cabecalho_externo.php'
     <!-- Terceiro destaque mobile/desktop-->
     <section class="container-flex w-100">
 
-<<<<<<< Updated upstream
+
         <img class="d-md-none d-flex w-75 m-auto" src="img/img-equip-desenvolvendo.png" alt="banner tudo o que você precisa esta aqui">
 
-        <article class="d-md-none align-center p-3 text-center bg-info pt-5 img-dois-avatar">
+        <article class="d-md-none align-center p-3 text-center bg-info pt-5 ">
             <h2>
                 Encontre freelancers!
-=======
-    <section class="container-flex ">
-        <img class="d-flex w-75 m-auto" src="img/img-projetando.png" alt="banner tudo o que você precisa esta aqui">
-
-        <article class="align-center p-3 text-center bg-verde pt-5 imgcasal faixa-meio">
-            <h2>
-                Comece agora!2
->>>>>>> Stashed changes
             </h2>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-            </p>
-        
-<<<<<<< Updated upstream
-            <button class="btn btn-sm btn-marinho mt-3 px-5 fs-5 align-center bold-button rounded-pill" type="button">Saiba +</button>
-=======
-            <a href="#">Saiba Mais +</a>
->>>>>>> Stashed changes
+    
         </article>
     </section>
     <!-- Fim terceiro destaque mobile/desktop -->
 
     <!-- Cards  -->
-    <section class="container" id="teste">
+    <section class="container content-center text-center" id="">
         <div class="row m-1">
 
             <h2 class="text-center my-5">Encontre Freelancers</h2>
 
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
+            <div class="card col-6 col-lg-4 mb-2" style="width: 18rem;">
+                <img src="img/img-notebook-cima-2.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-info rounded-pill">Go somewhere</a>
+                        <h5 class="card-title">Programação</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div>
             </div>
 
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
+            <div class="card col-6 col-lg-4 mb-2" style="width: 18rem;">
+                <img src="img/img-mulher-designer.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-info rounded-pill">Go somewhere</a>
+                        <h5 class="card-title">Design</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div>
             </div>
 
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
+            <div class="card col-6 col-lg-4 mb-2" style="width: 18rem;">
+                <img src="img/img-programadora-de-costas.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-info rounded-pill">Go somewhere</a>
+                        <h5 class="card-title">Segurança da Informação</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div>
             </div>
 
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
+            <div class="card col-6 col-lg-4 mb-2" style="width: 18rem;">
+                <img src="img/img-seguranca-informacao.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-info rounded-pill">Go somewhere</a>
+                        <h5 class="card-title">Ciência de dados</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div>
             </div>
 
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
+            <div class="card col-6 col-lg-4 mb-2" style="width: 18rem;">
+                <img src="img/img-equipe.jpg" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-info rounded-pill">Go somewhere</a>
+                        <h5 class="card-title">Coordenação de Equipe</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div>
             </div>
 
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-info rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
+            <div class="card col-6 col-lg-4 mb-2" style="width: 18rem;">
+            <img src="img/img-contadora.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Finanças</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
+            </div>
+
         </div>
     </section>
     <!-- Final cards -->
 
-<<<<<<< Updated upstream
    <?php require_once 'inc/footer_externo.php'; ?>
-=======
+
     <footer class="container-flex bg-footer">
 
     <div class="container text-center bg-footer">
@@ -264,37 +180,7 @@ require_once 'inc/cabecalho_externo.php'
 </div>
         
 
-        <article class="align-center p-3 text-center bg-marinho pt-5 imgcasal">
-            <h2>
-                Nome da empresa
-            </h2>
-            
-            <article class="align-center p-3 text-center bg-marinho pt-5 imgcasal">
-                <h4>
-                    Redes sociais
-                </h4>
-
-                <span>
-                    <i class="bi bi-instagram"></i>
-                </span>
-
-                <span>
-                    <i class="bi bi-twitter"></i>
-                </span>
-            </article>
         
-            
-        </article>
-        <article class="align-center p-3 text-center bg-marinho-secondary pt-5 imgcasal">
-            <h2>
-                Comece agora!
-            </h2>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-            </p>
-        </article>
-    </footer>
-
 </body>
 </html>
->>>>>>> Stashed changes
+

bb533864164fc1463ae7b333a911ffb0335918d3
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-08-10 09:49:08 -0300

    atualização vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '440311f2019f5aa136bc4be85ba00f4359b45f60',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '440311f2019f5aa136bc4be85ba00f4359b45f60',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

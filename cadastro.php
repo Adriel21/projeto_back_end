7b0fd5557e988c725de560b2addaa92a29321b29
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-08-22 11:41:39 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -19,46 +19,46 @@
 <body class="login-body">
     <main class="container text-center">
             <div class="row mt-3 ">
-            <div class="col-4 align-self-end destaque-login">
-                <h1 >XPTO.com</h1>
-                <br>
-                <h4 class="text-start">Entre na XPTO, e contemple nossos beneficíos</h4>
-            <p class="text-start">Realize o login para uma melhor experiência</p>
-                <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo nostrum aliquam voluptatibus. Cumque, nulla?</p>
-            
-                
-
-                <p class="col-3"><img src="img/img-projetando.png" alt=""></p>
-            </div>
-            
-            <div class="col">
-            <form class="formulario">
-                <div class="row">
-                    <p class="text-start"><strong> Fazer login com </strong></p>
-                    <br><br>
-                <p class="col botao-google"><a href="#">Google</a></p>
-                <p class="col botao-linkedin"><a href="#">LinkedIn</a></p>
-                </div>
-                <div class="mb-3">
+                <div class="col-4 align-self-end destaque-login">
+                    <h1 >XPTO.com</h1>
                     <br>
-                    <label for="exampleInputEmail1" class="form-label ">Email </label>
-                    <input type="email" class="form-control rounded" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">Nunca compartilhe seu email com estranhos !</div>
-                </div>
-                <br>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Senha</label>
-                    <input type="password" class="form-control rounded" id="exampleInputPassword1">
-                </div>
-                <div class="mb-3 form-check">
+                    <h4 class="text-start">Entre na XPTO, e contemple nossos beneficíos</h4>
+                    <p class="text-start">Realize o login para uma melhor experiência</p>
+                    <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo nostrum aliquam voluptatibus. Cumque, nulla?</p>
+                
                     
-                </div>
-                <button type="submit" class="btn btn-primary">ENTRAR</button>
-            </form>
 
-            </div>
+                    <p class="col-3"><img src="img/img-projetando.png" alt=""></p>
+                </div>
             
-        </div>
+                <div class="col">
+                    <form class="formulario">
+                        <div class="row">
+                            <p class="text-start"><strong> Fazer login com </strong></p>
+                            <br><br>
+                        <p class="col botao-google"><a href="#">Google</a></p>
+                        <p class="col botao-linkedin"><a href="#">LinkedIn</a></p>
+                        </div>
+                        <div class="mb-3">
+                            <br>
+                            <label for="exampleInputEmail1" class="form-label ">Email </label>
+                            <input type="email" class="form-control rounded" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">Nunca compartilhe seu email com estranhos !</div>
+                        </div>
+                        <br>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Senha</label>
+                            <input type="password" class="form-control rounded" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3 form-check">
+                            
+                        </div>
+                        <button type="submit" class="btn btn-primary">ENTRAR</button>
+                    </form>
+
+                </div>
+            
+            </div>
     </main>
 
 </body>

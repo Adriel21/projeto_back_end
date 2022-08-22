604648ec9a675bbb15fc51818b07282050e4f708
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-08-22 10:29:23 -0300

    Exclusão de bootstrap não utilizado

---
+++ perfil_principal.php
@@ -0,0 +1,12 @@
+<?php require_once './inc/cabecalho_externo.php'; ?>
+
+
+<section aria-label="Seção de Perfil">
+    <h2 class="text-center">Bem-Vindo, Maicon!</h2>
+    
+    <p class="text-center bd-shadow imagem-perfil"><img src="" alt="" width="200px" class=""></p>
+
+    <button class="btn-marinho rounded-pill px-5 py-1 mt-2 bold-button d-flex text-center" type="button">Saiba +</button>
+</section>
+
+<?php require_once './inc/footer_externo.php'; ?>

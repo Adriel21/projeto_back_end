3c4eb762394a4d85862126e7ff5eec037993f6ce
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-08-24 22:56:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,16 +5,16 @@ require_once 'inc/cabecalho_externo.php'
 <main class="container marketing mt-5">
 
     <!-- Primeiro Blaco -->
-    <section class="row featurette pb-5">
+    <section class="row featurette my-5 py-3">
         <picture class="col-md-5">
             <img src="img/img-projetando.png" alt="">
         </picture>
     
-        <div class="col-md-7 m-auto py-5 mb-5">
+        <div class="col-md-7 m-auto pt-5">
             <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
             <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque sit commodi praesentium placeat odit aperiam explicabo deleniti. Provident mollitia corporis, aliquam sequi, deserunt iusto at optio quas officiis totam molestiae, neque et sunt! Inventore, soluta.</p>
 
-            <div class="d-grid gap-2 col-6 mx-auto">
+            <div class="d-grid gap-2 col-6 m-auto mt-5">
                 <button class="btn btn-primary rounded" type="button">Cadastra-se</button>
             </div>
         </div>
@@ -24,7 +24,7 @@ require_once 'inc/cabecalho_externo.php'
     <!-- Segundo Bloco -->
     <hr class="featurette-divider d-none d-lg-block">
 
-    <div class="row featurette py-5">    
+    <div class="row featurette py-5 reverse">    
         
         <picture class="col-md-5 img-margin-nagativa">
             <img src="img/img-projetando.png" alt="">

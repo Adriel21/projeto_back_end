4748d9691305db708463dc17ba78a2563a5ea10c
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-08-26 12:03:35 -0300

    index finalizado

--- index.php
+++ index.php
@@ -2,59 +2,112 @@
 require_once 'inc/cabecalho_externo.php'
 ?>
 
-<main class="container marketing mt-5">
+<main class="container-sm-fluid marketing mt-5">
 
     <!-- Primeiro Blaco -->
-    <section class="row featurette my-5 py-3">
-        <picture class="col-md-5">
-            <img src="img/img-projetando.png" alt="">
-        </picture>
-    
+    <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-md-row">
         <div class="col-md-7 m-auto pt-5">
             <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
             <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque sit commodi praesentium placeat odit aperiam explicabo deleniti. Provident mollitia corporis, aliquam sequi, deserunt iusto at optio quas officiis totam molestiae, neque et sunt! Inventore, soluta.</p>
 
-            <div class="d-grid gap-2 col-6 m-auto mt-5">
-                <button class="btn btn-primary rounded" type="button">Cadastra-se</button>
+            <div class="d-grid gap-2 col-6 m-auto mt-5 rounded">
+                <button class="rounded-5" type="button">Cadastra-se</button>
             </div>
         </div>
-    </section>
-    <!-- Primeiro finalizado -->
 
-    <!-- Segundo Bloco -->
-    <hr class="featurette-divider d-none d-lg-block">
-
-    <div class="row featurette py-5 reverse">    
-        
-        <picture class="col-md-5 img-margin-nagativa">
-            <img src="img/img-projetando.png" alt="">
-        </picture>
-
-        <div class="col-md-7 m-auto bg-info p-5">
-            <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-            <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque sit commodi praesentium placeat odit aperiam explicabo deleniti. Provident mollitia corporis, aliquam sequi, deserunt iusto at optio quas officiis totam molestiae, neque et sunt! Inventore, soluta.</p>
-        </div>
-    </div>
-    <!-- Segundo bloco Finalizado -->
-
-    <hr class="featurette-divider d-none d-lg-block">
-
-    <div class="row featurette mt-5">
         <picture class="col-md-5">
             <img src="img/img-projetando.png" alt="">
         </picture>
-    
-        <div class="col-md-7 m-auto">
-            <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-            <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque sit commodi praesentium placeat odit aperiam explicabo deleniti. Provident mollitia corporis, aliquam sequi, deserunt iusto at optio quas officiis totam molestiae, neque et sunt! Inventore, soluta.</p>
-        </div>
-    </div>
+    </section>
+    <!-- Primeiro finalizado -->
 
-    <hr class="featurette-divider d-none d-lg-block">
 
-    <!-- /END THE FEATURETTES -->
+    <!-- Segundo Bloco -->
+    <section class="segna-bloco bg-action py-5">
+        <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-sm-row-reverse">
+            <div class="col-md-7 m-auto pt-5">
+                <h2 class="featurette-heading fw-normal lh-1">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque sit commodi praesentium placeat odit aperiam explicabo deleniti. Provident mollitia corporis, aliquam sequi, deserunt iusto at optio quas officiis totam molestiae, neque et sunt! Inventore, soluta.</p>
 
-  </div><!-- /.container -->
+                <div class="d-grid gap-2 col-6 m-auto mt-5 rounded">
+                    <button button class="rounded-5" type="button">Cadastra-se</button>
+                </div>
+            </div>
+
+            <picture class="col-md-5">
+                <img src="img/img-projetando.png" alt="">
+            </picture>
+        </section>
+    </section>
+    <!-- Final  Segundo bloco -->
+
+    <!-- cards Projetos -->
+
+    <section class="mt-5">
+        <h2 class="text-center text-muted">Cards Projetos</h2>
+            <section class="container row row-cols-1 row-cols-md-3 g-4 m-auto my-5">
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                    <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                    <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </section>
+
 </main>
    
 

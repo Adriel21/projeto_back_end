ce946e71bf544ab53216121a969b749c0ca38461
GabrielG9z <genovezgabriel2012@gmail.com>
2022-08-28 12:45:14 -0300

    Finalizado a pagina feed por hora
    Foi feito o css da página feed, por hora está terminado, ainda faltam alguns detalhes como footer e header que serão implementados pela equipe.

--- feed.php
+++ feed.php
@@ -11,12 +11,12 @@ require_once './inc/cabecalho-cadastro.php';
 
 <!-- Menu Lado direito -->
 
-<div class="container-fluid overflow-hidden menu-lateral">
-    <div class="row vh-100 overflow-auto">
+<div class="container-fluid overflow-hidden ">
+    <div class=" row vh-100 overflow-auto">
         <div class="col-12 col-sm-3 col-xl-2 px-sm-2 px-0 bg-light d-flex sticky-top">
-            <div class="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start px-3 pt-2 text-dark">
-                <a href="/" class="d-flex align-items-center pb-sm-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <span class="fs-5">Des<span class="d-none d-sm-inline">colajob</span></span>
+            <div class="menu-lateral d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start px-3 pt-2 text-dark">
+                <a href="/" class="d-flex align-items-center pb-sm-3 mb-md-0 me-md-auto  text-decoration-none">
+                    <span class="fs-5"><img src="img/logo-icone/logo_colajob-vetor-32.png" alt="Logo da colajob"><span class="ps-1 d-none d-sm-inline text-white">colajob</span></span>
                 </a>
                 <ul class="nav nav-pills flex-sm-column flex-row flex-nowrap flex-shrink-1 flex-sm-grow-0 flex-grow-1 mb-sm-auto mb-0 justify-content-center align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
@@ -28,8 +28,8 @@ require_once './inc/cabecalho-cadastro.php';
                          <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-sm-0 px-2">
                             <i class="fs-5 bi-speedometer2"></i><span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Categorias
+                                <a class="nav-link dropdown-toggle nav-link px-sm-0 px-2" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fs-5 bi bi-filter-square"></i>
+                                <span class="ms-1 d-none d-sm-inline">Categorias</span>
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     
@@ -73,31 +73,74 @@ require_once './inc/cabecalho-cadastro.php';
         <div class="col d-flex flex-column h-sm-100">
             <!-- Cabeçalho inicio -->
             <nav class="navbar container-fluid ">
-                <div class="cabecalho-interno d-flex col-12 pb-3  justify-content-center">
-                        <form class="d-flex p-1">
-                            <input class="form-control" type="search" placeholder="Digite o que procura" aria-label="Search">
-                            <button class="ps-2 btn btn-outline-success" type="submit">Search</button>
+                <div class="cabecalho-interno d-flex col-12 pb-3   justify-content-center">
+                        <form class="d-flex p-1 ">
+                            <input class="form-control " type="search" placeholder="Digite o que procura" aria-label="Search">
+                            <div class="ps-2">
+                            <button class="botao-feed ps-2 btn text-white" type="submit">BUSCAR</button>
+                            </div>
                         </form>
                 </div>
             </nav> 
             <!-- Cabeçalho fim -->
 
-            <main class="row overflow-auto">
-                
-                <div class="col pt-4 coluna-vagas">
-                    <h3>Vertical Sidebar that switches to Horizontal Navbar</h3>
-                    <p class="lead">An example multi-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single menu is be open at a time.</p>
-                    <hr />
-                    <h3>More content...</h3>
-                    <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
-                    <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+            <!-- Início conteúdo das vagas -->
 
-                    <h3>Vertical Sidebar that switches to Horizontal Navbar</h3>
-                    <p class="lead">An example multi-level sidebar with collasible menu items. The menu functions like an "accordion" where only a single menu is be open at a time.</p>
-                    <hr />
-                    <h3>More content...</h3>
-                    <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
-                    <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+            <main class="row overflow-auto border border-2 border-opacity-50 pb-2 rounded box-vagas">
+
+            
+            <div class="col pt-4 card-vagas">
+                <div class="card w-77">
+                    <div class="card-body coluna-vagas">
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h3 class="mb-1 pb-4">Oportunidade de emprego</h3>
+                        <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">Descrição do job de acordo com o pedido do cliente cadastrado.</p>
+                        <small>(Nome do cliente talvez.)</small>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="botao-feed btn   me-md-2" type="button">QUERO ME CANDIDATAR</button>
+                        
+                        </div>
+                    </a>
+                </div>
+                </div>
+                <div class="col pt-4 ">
+                <div class="card w-77">
+                    <div class="card-body coluna-vagas">
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h3 class="mb-1 pb-2">Oportunidade de emprego</h3>
+                        <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">Descrição do job de acordo com o pedido do cliente cadastrado.</p>
+                        <small>(Nome do cliente talvez.)</small>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="botao-feed btn   me-md-2" type="button">QUERO ME CANDIDATAR</button>
+                        
+                        </div>
+                    </a>
+                </div>
+                </div>
+
+                <div class="col pt-4 ">
+                <div class="card w-77">
+                    <div class="card-body coluna-vagas">
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h3 class="mb-1 pb-2">Oportunidade de emprego</h3>
+                        <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">Descrição do job de acordo com o pedido do cliente cadastrado.</p>
+                        <small>(Nome do cliente talvez.)</small>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="botao-feed btn   me-md-2" type="button">QUERO ME CANDIDATAR</button>
+                        
+                        </div>
+                    </a>
+                </div>
+                </div>
 
                     
                 </div>
@@ -106,13 +149,13 @@ require_once './inc/cabecalho-cadastro.php';
 
             <!-- Páginação das vagas -->
             <nav aria-label="Page navigation example">
-                <ul class="pagination justify-content-end">
-                    <li class="page-item disabled"><a class="page-link">Previous</a></li>
+                <ul class="pt-2 pagination justify-content-end align-items-end paginacao">
+                    <li class="page-item disabled"><a class="page-link">Anterior</a></li>
                     <li class="page-item"><a class="page-link" href="#">1</a></li>
                     <li class="page-item"><a class="page-link" href="#">2</a></li>
                     <li class="page-item"><a class="page-link" href="#">3</a></li>
                     <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
+                    <a class="page-link" href="#">Próxima</a>
                     </li>
                 </ul>
             </nav>
@@ -121,9 +164,7 @@ require_once './inc/cabecalho-cadastro.php';
             <!-- Footer -->
 
             
-            <footer class="row bg-light py-4 mt-auto">
-                <div class="col"> Footer content here... </div>
-            </footer>
+            
 
             <!-- Fim footer -->
         
--- inc/cabecalho-cadastro.php
+++ inc/cabecalho-cadastro.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto - back end</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="/img/logo-icone/favicon.ico">
+
     <!-- Css includes -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 

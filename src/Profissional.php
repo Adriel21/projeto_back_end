5f087bcc96f598d6d1572537b2e1aa45422e4099
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-08-28 11:38:38 -0300

    conexão com o novo banco

---
+++ src/Profissional.php
@@ -0,0 +1,155 @@
+<?php
+namespace Projeto;
+use PDO, Exception;
+
+final class Projeto{
+    private int $id;
+    private string $titulo;
+    private string $descricao;
+    private int $usuarioId;
+    private int $categoriaId;
+    private PDO $conexao;
+
+
+    public function __construct()
+    {
+        $this->conexao = Banco::conecta();
+    }
+
+    public function cadastrar():void {
+        $sql = "INSERT INTO profissioanl(titulo, descricao, usuario_id, categoria_id) VALUES(:titulo, :descricao, :usuario_id, :categoria_id)";
+        
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindParam(":titulo", $this->titulo, PDO::PARAM_STR);
+            $consulta->bindParam(":descricao", $this->descricao, PDO::PARAM_STR);
+            $consulta->bindParam(":usuario_id", $this->usuarioId, PDO::PARAM_INT);
+            $consulta->bindParam(":categoria_id", $this->categoriaId, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro: ". $erro->getMessage());
+        }
+    }
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of titulo
+     */ 
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @return  self
+     */ 
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descricao
+     */ 
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    /**
+     * Set the value of descricao
+     *
+     * @return  self
+     */ 
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of usuarioId
+     */ 
+    public function getUsuarioId()
+    {
+        return $this->usuarioId;
+    }
+
+    /**
+     * Set the value of usuarioId
+     *
+     * @return  self
+     */ 
+    public function setUsuarioId($usuarioId)
+    {
+        $this->usuarioId = $usuarioId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categoriaId
+     */ 
+    public function getCategoriaId()
+    {
+        return $this->categoriaId;
+    }
+
+    /**
+     * Set the value of categoriaId
+     *
+     * @return  self
+     */ 
+    public function setCategoriaId($categoriaId)
+    {
+        $this->categoriaId = $categoriaId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of conexao
+     */ 
+    public function getConexao()
+    {
+        return $this->conexao;
+    }
+
+    /**
+     * Set the value of conexao
+     *
+     * @return  self
+     */ 
+    public function setConexao($conexao)
+    {
+        $this->conexao = $conexao;
+
+        return $this;
+    }
+}
+    ?>

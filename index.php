4de8cc4675db89ac380a217b1a28e1049150afdf
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-08-29 03:25:08 -0300

    att index

--- index.php
+++ index.php
@@ -1,135 +1,155 @@
 <?php 
 require_once 'inc/cabecalho_externo.php'
- 
 ?>
 
-    <!-- Primeiro destaque -->
-    
-    <section class="container my-5 justify-content-center media-break-point sm">
+<main class="container-sm-fluid marketing mt-5">
 
-        <picture class="d-sm-none justify-content-center">
-           <p> 
-               <img class="d-flex w-75 m-auto" src="img/img-projetando.png" alt="banner tudo o que você precisa esta aqui">
-           </p>
+    <!-- Primeiro Blaco -->
+    <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-md-row">
+            <div class="col-xl-6 col-lg-6 col-md-12 m-auto">
+                <h1 class="display-4 fw-bold pe-lg-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, architecto.</h1>
+                    <!-- text -->
+                    <p class="mb-4 lead">
+                        If you’re passionate and ready to dive in, we’d love to meet you. We’re committed to supporting our
+                         employee professional development and well-being.
+                    </p>
+
+                <div class="d-grid gap-2 col-4 mt-5 rounded">
+                    <button button class="rounded-5 py-2 border-none fs-5" type="button">
+                        Cadastra-se
+                    </button>
+                </div>
+            </div>
+
+        <picture class="col-md-5 text-center">
+            <img class="img-responsive m-auto" src="img/img-pessoas-sinalizando.png" alt="" width="100%" height="100%">
         </picture>
+    </section>
+    <!-- Primeiro finalizado -->
 
-    <main class="container text-center">
-  
-        <div class="row align-items-center ">
-            
-        <div class="col d-none d-md-block d-print-block col-md-8">
-            One of three columns
-        </div>
-        
-        <aside class="col d-none d-md-block d-print-block col-md-4">
-            <p><img src="img/img-job-anotacao.png" alt=""></p>
-        </aside>
-    </div>
-  
-    </main>
 
-        <div class="align-center p-3 text-center">
-            <h1>
-                Lorem ipsum dolor sit amet.
-            </h1>
+    <!-- Segundo Bloco -->
+    <section class="segna-bloco bg-primary py-2">
+        <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-sm-row-reverse">
+            <div class="col-xl-6 col-lg-6 col-md-12 m-auto">
+                <h2 class="text-white display-4 fw-bold pe-lg-8">Join the Geeks team &amp; shape the future of design</h2>
+                    <!-- text -->
+                    <p class="text-white mb-4 lead">
+                        If you’re passionate and ready to dive in, we’d love to meet you. We’re committed to supporting our
+                        employee professional development and well-being.
+                    </p>
 
-            <h2>
-                Ola mundo
-            </h2>
+                    <div class="d-grid gap-2 col-4 mt-5 rounded">
+                        <button button class="rounded-5 py-2 border-none fs-5" type="button">
+                            Cadastra-se
+                        </button>
+                    </div>
+            </div>
 
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium nesciunt aliquam atque similique inventore quibusdam, consequuntur esse unde rem quae ducimus distinctio nostrum ad rerum.
-            </p>
-        
-            <button class="btn btn-sm btn-saibamais mt-3 px-5 fs-5 align-center bold-button rounded-pill" type="button">Começa agora!</button>
+            <picture class="col-md-5 text-lg-end text-center">
+                <img class="img-responsive m-auto" src="img/img-pessoas-olhando-celular.png" alt="" width="100%" height="100%">
+            </picture>
+        </section>
+    </section>
+    <!-- Final  Segundo bloco -->
+
+    <!-- cards Projetos -->
+
+    <section class="mt-5">
+        <h2 class="text-center text-muted">Cards Projetos</h2>
+            <section class="container row row-cols-1 row-cols-md-3 g-4 m-auto my-5">
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 p-2 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                    <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 p-2 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                    <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 p-2 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 p-2 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 p-2 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card h-100 shadow">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <button class="rounded-5 p-2 px-4" type="button">Cadastra-se</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </section>
+
+    <section class="bg-primary">
+        <div class="container py-5">
+            <!-- row -->
+            <div class="row align-items-center g-0">
+                <div class="col-xl-6 col-lg-6 col-md-12 m-auto">
+                    <!-- heading -->
+                    <div>
+                        <h1 class="text-white display-4 fw-bold pe-lg-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                        <!-- text -->
+                        <p class="text-white-50 mb-4 lead">
+                            If you’re passionate and ready to dive in, we’d love to meet you. We’re committed to supporting our
+                            employee professional development and well-being.
+                        </p>
+                        <!-- btn -->
+                        <div class="d-grid gap-2 col-4 mt-5 rounded">
+                            <button button class="rounded-5 py-2 border-none fs-5" type="button">
+                                Cadastra-se
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- img -->
+                <picture class="col-xl-6 col-lg-6 col-md-12 text-lg-end text-center pt-5">
+                    <img class="img-responsive m-auto" src="img/img-equipe-conversando.png" alt="" width="100%" height="100%">
+                </picture>
+            </div>
         </div>
     </section>
-    <!-- Final Primeiro destaque -->
 
-    <!-- Segunda destaque -->
-    <section class="container-flex">
-        <img class="d-flex w-75 m-auto" src="img/img-equip-desenvolvendo.png" alt="banner tudo o que você precisa esta aqui">
+</main>
+   
 
-        <article class="align-center text-center py-5 img-dois-avatar">
-            <h2>
-                Comece agora!
-            </h2>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-            </p>
-        
-            <button class="btn-marinho rounded-pill px-5 py-1 mt-2 bold-button" type="button">Saiba +</button>
-        </article>
-    </section>
-    <!-- Final segundo destaque -->
-
-    <!-- Cards  -->
-    <section class="container" id="teste">
-        <div class="row m-1">
-
-            <h2 class="text-center my-5">Encontre Freelancers</h2>
-
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-saibamais rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-saibamais rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-saibamais rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-saibamais rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><i class="bi bi-laptop mx-2"></i>Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-saibamais rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-6 col-lg-4 mb-2">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-saibamais rounded-pill">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Final cards -->
-
-   <?php require_once 'inc/footer_externo.php'; ?>
+<?php require_once 'inc/footer_externo.php'; ?>

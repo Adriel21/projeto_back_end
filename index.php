674db8a7584b7c12517fd280a8c32a3fea1cdc1a
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-09-05 11:11:48 -0300

    Ajustes do index no tablet

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'inc/header.php'
 <main class="container-sm-fluid marketing mt-5">
 
     <!-- Primeiro Blaco -->
-    <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-md-row">
+    <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-lg-row">
             <div class="col-xl-6 col-lg-6 col-md-12 m-auto">
                 <h1 class="display-4 fw-bold pe-lg-8">Encontre projetos, amplie sua rede e gere renda extra.</h1>
                     <!-- text -->
@@ -13,13 +13,15 @@ require_once 'inc/header.php'
                         Com a Colajob, você poderá encontrar muitos projetos que irão contribuir para o seu bolso e para ampliar seu portfólio. Aproveite a oportunidade e, veja se há algum que se encaixa com você!
                     </p>
 
-                <div class="d-grid gap-2 col-6 col-sm-4 mt-5 rounded">
-                <button class="botao-feed btn me-md-2 p-2" type="button">Procurar Projetos</button>
+                <div class="d-grid gap-2 col-6 col-sm-4 col-md-6 mt-5 rounded">
+                <button class="botao_index btn me-md-2 p-2" type="button"><a href="projetos.php">Procurar Projetos</a></button>
                 </div>
             </div>
 
-        <picture class="col-md-5 text-center">
-            <img class="img-responsive m-auto" src="img/img-pessoas-sinalizando.png" alt="" width="100%" height="100%">
+    
+          <!-- img -->
+        <picture class="col-xl-6 col-lg-6 col-md-12 text-lg-end text-center pt-1">
+          <img class="img-responsive m-auto" src="img/img-pessoas-sinalizando.png" alt="" width="100%" height="100%">
         </picture>
     </section>
     <!-- Primeiro finalizado -->
@@ -27,7 +29,7 @@ require_once 'inc/header.php'
 
     <!-- Segundo Bloco -->
     <section class="segna-bloco py-2" style="background-color: #0421B5;">
-        <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-sm-row-reverse">
+        <section class="container row featurette my-5 py-3 m-auto flex-column-reverse flex-lg-row-reverse">
             <div class="col-xl-6 col-lg-6 col-md-12 m-auto">
                 <h2 class="text-white display-4 fw-bold pe-lg-8">Profissionais diversos prontos para te ajudar.</h2>
                     <!-- text -->
@@ -35,13 +37,13 @@ require_once 'inc/header.php'
                         Precisando de ajuda qualificada? Está no lugar certo. Na Colajob você pode conseguir quanta ajuda for necessária para concluir seus projetos.
                     </p>
 
-                    <div class="d-grid gap-2 col-6 col-sm-4 mt-5 rounded">
-                    <button class="botao-feed btn me-md-2" type="button">Procurar Freelancers</button>
+                    <div class="d-grid gap-2 col-6 col-sm-4 col-md-6 mt-5 rounded">
+                    <button class="botao_index btn me-md-2" type="button"><a href="freelancers.php">Procurar Freelancers</a></button>
                     </div>
             </div>
-
-            <picture class="col-md-5 text-lg-end text-center">
-                <img class="img-responsive m-auto" src="img/img-pessoas-olhando-celular.png" alt="" width="100%" height="100%">
+              <!-- img -->
+            <picture class="col-xl-6 col-lg-6 col-md-12 text-lg-end text-center pt-1">
+              <img class="img-responsive m-auto" src="img/img-pessoas-olhando-celular.png" alt="" width="100%" height="100%">
             </picture>
         </section>
     </section>
@@ -58,7 +60,7 @@ require_once 'inc/header.php'
                             <h5 class="card-title">Desenvolvedores</h5>
                             <img class="card-img-top mb-2" src="img/cards_index/desenvolvedores.jpg" alt="">
                             <p class="card-text">Aqui, você pode encontrar a ajuda necessária para desenvolver seu site ou aplicativo.</p>
-                            <button class="botao-feed btn me-md-2" type="button">
+                            <button class="botao_index btn me-md-2" type="button">
                                 <a class="text-white" href="feed.php?id=1">    
                                     Ver Freelancers
                                 </a>
@@ -73,7 +75,7 @@ require_once 'inc/header.php'
                             <h5 class="card-title">Designers</h5>
                             <img class="card-img-top mb-2" src="img/cards_index/designers.jpg" alt="">
                             <p class="card-text">Encontre designers incriveis para construirem a identidade que você deseja.</p>
-                            <button class="botao-feed btn   me-md-2" type="button">Ver Freelancers</button>
+                            <button class="botao_index btn   me-md-2" type="button">Ver Freelancers</button>
                         </div>
                     </div>
                 </div>
@@ -84,7 +86,7 @@ require_once 'inc/header.php'
                             <h5 class="card-title">Redatores</h5>
                             <img class="card-img-top mb-2" src="img/cards_index/redatores.jpg" alt="">
                             <p class="card-text">Precisando de alguém que elabore conteúdo para você? Que tal buscar um redator?</p>
-                            <button class="botao-feed btn   me-md-2" type="button">Ver Freelancers</button>
+                            <button class="botao_index btn   me-md-2" type="button">Ver Freelancers</button>
                         </div>
                     </div>
                 </div>
@@ -95,7 +97,7 @@ require_once 'inc/header.php'
                             <h5 class="card-title">Fotógrafos</h5>
                             <img class="card-img-top mb-2" src="img/cards_index/fotografos.jpg" alt="">
                             <p class="card-text">Um bom fotógrafo pode te auxiliar a registrar momentos importantes da sua vida.</p>
-                            <button class="botao-feed btn   me-md-2" type="button">Ver Freelancers</button>
+                            <button class="botao_index btn   me-md-2" type="button">Ver Freelancers</button>
                         </div>
                     </div>
                 </div>
@@ -106,7 +108,7 @@ require_once 'inc/header.php'
                             <h5 class="card-title">Consultores</h5>
                             <img class="card-img-top mb-2" src="img/cards_index/consultores.jpg" alt="">
                             <p class="card-text">Uma boa consultoria pode ajudar o seu negócio a alcançar o objetivo que deseja.</p>
-                            <button class="botao-feed btn   me-md-2" type="button">Ver Freelancers</button>
+                            <button class="botao_index btn   me-md-2" type="button">Ver Freelancers</button>
                         </div>
                     </div>
                 </div>
@@ -117,7 +119,7 @@ require_once 'inc/header.php'
                             <h5 class="card-title">Professores</h5>
                             <img class="card-img-top mb-2" src="img/cards_index/professores.jpg" alt="">
                             <p class="card-text">Gostaria de algumas aulas de reforço? Que tal encontrar um professor para te ajudar?</p>
-                            <button class="botao-feed btn   me-md-2" type="button">Ver Freelancers</button>
+                            <button class="botao_index btn   me-md-2" type="button">Ver Freelancers</button>
                         </div>
                     </div>
                 </div>
@@ -127,10 +129,10 @@ require_once 'inc/header.php'
   
 </main>
    
-<section class="segna-bloco py-2" style="background-color: #0421B5;">
+<section class="segna-bloco py-1 px-3" style="background-color: #0421B5;">
         <section class="container row featurette my-5 py-3 m-auto">
             <!-- row -->
-            <div class="row align-items-center g-0 flex-column-reverse flex-sm-row">
+            <div class="row align-items-center g-0 flex-column-reverse flex-lg-row">
                 <div class="col-xl-6 col-lg-6 col-md-12 m-auto">
                     <!-- heading -->
                     <div>
@@ -140,13 +142,13 @@ require_once 'inc/header.php'
                             Sabemos o quanto os tempos estão difíceis. Por isso, a Colajob possui como missão facilitar a busca por trabalhos, trazendo para o público a oportunidade de atuarem de uma das formas que mais vem crescendo ultimamente - como Freelancers.
                         </p>
                         <!-- btn -->
-                        <div class="d-grid gap-2 col-6 col-sm-4 mt-5 rounded">
-                            <button class="botao-feed btn me-md-2" type="button">Saiba Mais</button>
+                        <div class="d-grid gap-2 col-6 col-sm-4 col-md-6 mt-5 rounded">
+                            <button class="botao_index btn me-md-2" type="button">Saiba Mais</button>
                         </div>
                     </div>
                 </div>
                 <!-- img -->
-                <picture class="col-xl-6 col-lg-6 col-md-12 text-lg-end text-center pt-5">
+                <picture class="col-xl-6 col-lg-6 col-md-12 text-lg-end text-center pt-1">
                     <img class="img-responsive m-auto" src="img/img-equipe-conversando.png" alt="" width="100%" height="100%">
                 </picture>
             </div>

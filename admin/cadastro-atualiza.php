ce3576ae5a6440e09ae6e3aae6757832c4bd6c26
GabrielG9z <genovezgabriel2012@gmail.com>
2022-09-05 13:28:41 -0300

    adição de js, testes

--- admin/cadastro-atualiza.php
+++ admin/cadastro-atualiza.php
@@ -52,18 +52,26 @@ if(isset($_POST['atualizar'])) {
 
 
 <!--criando formulario de cadastro -->
-<div class="container col-md-12 col-sm-12 marketing shadow rounded">
+<div class="container col-md-12 col-sm-10 marketing shadow rounded">
 		<div class="row justify-content-center featurette my-5 p-sm-5">
         <h1 class="ps-5 pt-2 py-2 cta-formulario-atualiza">Atualize seus dados</h1>
 			<div class="col-12 col-md-8 col-sm-12  p-sm-4 p-4 ">
+			
+			
 				<form enctype="multipart/form-data" class="formulario-atualiza form-horizontal bg-form  p-sm-5 p-5 my-1 rounded" action="" method="POST">
+
+				<!-- Campo de inserção de imagem -->
+				<div class="border-shadow img-vazia "><img src="/img/placeholder-img-vazio.png" id="img"
+					alt="" class="d-block mx-auto mb-3" width="140" height="170">	
+				</div>
+			<!-- Fim campo de inserção de imagem -->
 					
 				<div class="form-group mt-2">
 						<label for="nome">Nome</label>
 						<input type="text" class="form-control" id="nome" value="<?=$dados['nome']?>" name="nome" placeholder="Nome" required>
 					</div>
 
-					<div class="form-group pb-3  mt-2">
+					<div class="form-group pb-3  mt-4">
 						<label for="email" class="pb-1">Email</label>
 						<input type="email" class="form-control" id="email" name="email" placeholder="Email" value="<?=$dados['email']?>">
 					</div>
@@ -76,15 +84,15 @@ if(isset($_POST['atualizar'])) {
 						<input type="password" class="form-control" id="senha" name="senha" placeholder="Senha">
 					</div>
 
-					<div class="mb-3">
-                <label for="imagem-existente" class="form-label">Imagem da notícia:</label>
+					<div class="mt-4 pb-3">
+                <label for="imagem-existente" class="form-label">Foto de perfil:</label>
                 <!-- campo somente leitura, meramente informativo -->
                 <input class="form-control" value="<?=$dados['perfil']?>" type="text" id="imagem-existente" name="perfil-existente" readonly>
             	</div>
 
             <div class="mb-3">
                 <label for="imagem" class="form-label">Caso queira mudar, selecione outra imagem:</label>
-                <input class="form-control" type="file" id="imagem" name="perfil" accept="image/png, image/jpeg, image/gif, image/svg+xml">
+                <input class="form-control" type="file" name="upload" accept="image/png, image/jpeg, image/gif, image/svg+xml" id="upload">
             </div>
 
 					
@@ -93,6 +101,10 @@ if(isset($_POST['atualizar'])) {
                     <button class="botao-feed btn text-white" type="submit" name="atualizar">ATUALIZAR</button>
                     </div>
 				</form>
+
+				<script src="js.js"></script>
 			</div>
 		</div>
 	</div>
+
+	

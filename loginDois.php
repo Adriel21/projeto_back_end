98a94fbadffc955c5d2035b7fc2da4d2f5067c00
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-09-06 23:48:39 -0300

    LoginDois css

--- loginDois.php
+++ loginDois.php
@@ -9,19 +9,17 @@ require "./vendor/autoload.php";
 
 <?php require_once 'inc/headerValidacao.php' ?>
 
-	<div class="container marketing shadow">
+	<div class="container marketing shadow shadow-lg">
 		<div class="row featurette my-5 p-sm-5">
 
-			<div class="col-md-6 d-none d-md-block">
-				<h1 >XPTO.com</h1>
-                    <h4 class="text-start">Entre na XPTO, e contemple nossos beneficíos</h4>
-                    <p class="text-start">Realize o login para uma melhor experiência</p>
-                    <p class="text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nemo nostrum aliquam voluptatibus. Cumque, nulla?</p>
+			<div class="col-md-6 d-none d-md-block pt-5">
+                    <h2 class="text-start titulo_login">Juntos, podemos fazer mais!</h2>
+                    <p class="text-start">Realize o login para uma melhor experiência.</p>
 					<img class="d-flex w-50 m-auto" src="img/img-mexendo-no-celular.png">
 			</div>
 
 			<div class="col-md-6 p-sm-5 p-4">
-				<form class="form-horizontal bg-form p-sm-5 p-3 rounded" action="" method="POST">
+				<form class="form-horizontal shadow-lg bg-form shadow p-sm-5 p-3 rounded" action="" method="POST">
 					<div class="d-grid gap-2 text-center">
 						<button class="btn btn-primary google-button" type="button">Google</button>
 						<button class="btn btn-primary" type="button">LinkedIn</button>
@@ -54,7 +52,7 @@ require "./vendor/autoload.php";
 
           
                     <div class="d-grid gap-2 text-center">
-					    <button type="submit" class="btn btn-primary mt-3" name="entrar">Entra</button>
+					    <button type="submit" class="botao_index btn mt-3" name="entrar">Entra</button>
                         <a href="cadastro.php">Ainda não criou sua conta? Cadastre-se</a>
                     </div>
 				</form>
